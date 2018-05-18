624cb3d3aeb8231765529358865d468444493a5f
papalouis <papa.louis59@gmail.com>
2018-05-18 17:35:58 +0200

    form advance

--- src/com/form.java
+++ src/com/form.java
@@ -1,6 +1,8 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 
 public class form extends HttpServlet {
     public static final String VUE = "/WEB-INF/inscription.jsp";
+    public static final String CHAMP_EMAIL = "email";
+    public static final String CHAMP_PASS = "motdepasse";
+    public static final String CHAMP_CONF = "confirmation";
+    public static final String CHAMP_NOM = "nom";
+    public static final String ATT_ERREURS  = "erreurs";
+    public static final String ATT_RESULTAT = "resultat";
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
         /* Affichage de la page d'inscription */
@@ -16,6 +24,80 @@ public class form extends HttpServlet {
     }
 
     public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-        /* Traitement des données du formulaire */
+        /* Récupération des champs du formulaire. */
+        String resultat;
+        Map<String, String> erreurs = new HashMap<String, String>();
+
+        String email = request.getParameter( CHAMP_EMAIL );
+        String motDePasse = request.getParameter( CHAMP_PASS );
+        String confirmation = request.getParameter( CHAMP_CONF );
+        String nom = request.getParameter( CHAMP_NOM );
+
+        /* Validation du champ email. */
+        try {
+            validationEmail( email );
+        } catch ( Exception e ) {
+            erreurs.put( CHAMP_EMAIL, e.getMessage() );
+        }
+
+        /* Validation des champs mot de passe et confirmation. */
+        try {
+            validationMotsDePasse( motDePasse, confirmation );
+        } catch ( Exception e ) {
+            erreurs.put( CHAMP_PASS, e.getMessage() );
+        }
+
+        /* Validation du champ nom. */
+        try {
+            validationNom( nom );
+        } catch ( Exception e ) {
+            erreurs.put( CHAMP_NOM, e.getMessage() );
+        }
+
+        /* Initialisation du résultat global de la validation. */
+        if ( erreurs.isEmpty() ) {
+            resultat = "Succès de l'inscription.";
+        } else {
+            resultat = "Échec de l'inscription.";
+        }
+
+        /* Stockage du résultat et des messages d'erreur dans l'objet request */
+        request.setAttribute( ATT_ERREURS, erreurs );
+        request.setAttribute( ATT_RESULTAT, resultat );
+
+        /* Transmission de la paire d'objets request/response à notre JSP */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+
+    private void validationEmail( String email ) throws Exception{
+        if ( email != null && email.length() != 0 ) {
+            if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
+                throw new Exception( "Merci de saisir une adresse mail valide." );
+            }
+        } else {
+            throw new Exception( "Merci de saisir une adresse mail." );
+        }
+    }
+
+    private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception{
+        if (motDePasse != null && motDePasse.trim().length() != 0 && confirmation != null && confirmation.trim().length() != 0) {
+            if (!motDePasse.equals(confirmation)) {
+                throw new Exception("Merci de confirmer avec le bon mot de passe.");
+            } else if (motDePasse.trim().length() < 3) {
+                throw new Exception("Les mots de passe doivent contenir au moins 3 caractères.");
+            }
+        } else {
+            throw new Exception("Merci de saisir et confirmer votre mot de passe.");
+        }
+    }
+
+    private void validationNom( String nom ) throws Exception{
+        if (nom != null && nom.trim().length() != 0) {
+            if (nom.trim().length() < 3) {
+                throw new Exception("Nom trop court.");
+            }
+        }else{
+            throw new Exception("Merci de rentrer un nom.");
+        }
     }
 }
